d44f045f9e7c1632ffdd56121060870d8618715a
“Sif <sif_aagaard_svendsen@hotmail.com>
2019-10-25 09:32:47 +0200

    wp-startertheme style.css

--- web/mortenstema/sidebar-footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Footer widgets
-?>
-<div id="footer-widget-area" role="complementary">
-<?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
- 
-<?php dynamic_sidebar( 'footer-sidebar-area' ); ?>
-<?php endif; ?>
-</div>
